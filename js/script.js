055872c7bb82df526073fd922f6eb8a95472981c
amareiit <161231530+amareiit@users.noreply.github.com>
2024-11-14 11:06:06 -0600

    fixing javascript

--- js/script.js
+++ js/script.js
@@ -1,27 +1,8 @@
-function initMap() {
-  const mapOptions = {
-    center: { lat: 41.484, lng: -87.728 },
-    zoom: 12,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    streetViewControl: true,
-    mapTypeControl: true,
-    minZoom: 10,
-    maxZoom: 16
-  };
-
-  const map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-  const marker = new google.maps.Marker({
-    position: { lat: 41.484, lng: -87.728 },
-    map: map,
-    title: "Southland College Prep"
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
   });
-
-  const infoWindow = new google.maps.InfoWindow({
-    content: "<p>My former high school I graduated from.</p>"
-  });
-
-  marker.addListener("click", function() {
-    infoWindow.open(map, marker);
-  });
-}
+});
