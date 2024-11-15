8d88770c098deff4427977b7fd4bd1b717724aaf
amareiit <161231530+amareiit@users.noreply.github.com>
2024-11-15 03:14:56 -0600

    final html updates

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
+  anchor.addEventListener('click', function (e) {
+    const targetId = this.getAttribute('href').substring(1); 
+    const targetElement = document.getElementById(targetId); 
+
+    if (targetElement) {
+      e.preventDefault();
+      targetElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    }
   });
 });
+
 document.getElementById('current-year').textContent = new Date().getFullYear();
